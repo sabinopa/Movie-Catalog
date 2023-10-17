6981ec7657ea5e8bf7a98d4e3106d90406020609
sabinopa <priasabino@gmail.com>
2023-10-17 20:45:02 -0300

    fix: ajusta links da home

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,8 @@
 class DirectorsController < ApplicationController 
+    def index
+        @directors = Director.all
+    end
+
     def new
         @director = Director.new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  resources :movies, only: [:show, :new, :create, :edit, :update] do
+  resources :movies do
       patch :publish, on: :member
   end
-  resources :genres, only:  [:show, :new, :create, :edit, :update]
-  resources :directors, only:  [:show, :new, :create, :edit, :update]
+  resources :genres
+  resources :directors
 end
